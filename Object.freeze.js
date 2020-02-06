fd22d653f0741b6b4d9a83ced27a5a4ab34e932b
Oleksii Solomakha <alexssolomakha@gmail.com>
2020-02-06 17:26:31 +0200

    Object.freeze

--- Object.freeze.js
+++ Object.freeze.js
@@ -1,4 +1,4 @@
-function objectFreeze(obj) {
+Object.prototype.objectFreeze = function (obj) {
   let freezedObj = {};
   for(let key in obj){
     freezedObj = Object.defineProperty(obj, key, {writable: false, configurable: false});
