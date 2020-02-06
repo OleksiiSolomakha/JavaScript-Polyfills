1e638b14edd85afd27ea006a71a6c1be941fc7d8
Oleksii Solomakha <alexssolomakha@gmail.com>
2020-02-06 14:47:45 +0200

    Object.create

---
+++ Object.create.js
@@ -0,0 +1,13 @@
+function objectCreate (obj, props = null) {
+  function Create() {};
+  Create.prototype = obj;
+  const newObject  = new Create();
+  if (typeof(props) === "object") {
+    for (prop in props) {
+     if (props.hasOwnProperty((prop))) {
+      newObject[prop] = props[prop].value;
+     }
+    }
+   }
+  return newObject;
+}
